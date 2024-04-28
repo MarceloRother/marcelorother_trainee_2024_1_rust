d5599bf683436554d36f9b695cd1661add6d63f7
MARCELO <marcelorotherwork@gmail.com>
2024-04-27 22:56:15 -0300

    ajusta main e cria cabeçalho

--- src/jogo.rs
+++ src/jogo.rs
@@ -5,3 +5,14 @@ pub struct Jogo{
     carteiro: Carteiro,
     caixa: Caixa
 }
+
+impl Jogo {
+    pub fn joga(&self){
+    
+    }
+
+    pub fn cria_jogo(matriz: [[char; 3] ; 3]){
+
+    }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use jogo::Jogo;
+
+//  JOGO DA ENTREGA
+//
+// O jogo consiste em fazer com que o carteiro ('&') leve a caixa ('@') até ó ponto desejado ('X') em um campo que será uma matriz 20x20, onde ('+') representa um espaço válido..
+// Para desenvolver tal projeto vocês terão de usar/desenvolver as estruturas 'carteiro', 'caixa', e 'jogo'.
+// Obs:
+//   - O carteiro só pode andar um 'índice' por iteração
+//   - Apliquem a ideia de Encapsulamento
+//   - O código tera uma mapa de exemplo para o teste enquanto estiver em desenvolvimento
+//   - No dia da apresentação o código será posto em prática com um código diferente 
+mod carteiro;
+mod caixa;
+mod jogo;
+ 
 fn main() {
-    println!("Hello, world!");
+    let matriz: [[char; 20]; 20];
+
+    let jogo: Jogo;
+    jogo.cria_ jogo();
 }
